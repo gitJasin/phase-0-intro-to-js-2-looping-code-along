f26ae59f2fb8c905cd6dbf79ae5c6342722b79a7
Jason Stewart <jaysin.stewart@gmail.com>
2023-03-07 16:30:52 -0800

    Completed Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy biorthday to me!`);
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(names, eventName) {    
+    
+    const cards = [];  
+    
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);    
+    }
+    
+    return cards;
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown (positiveInt) {
+    let i = positiveInt;
+    
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
